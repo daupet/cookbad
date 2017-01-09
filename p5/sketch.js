a385d04a384e100c222fc4e98a89df2ecd5a3ecf
daupet <ptjpwm992@gmail.com>
2017-01-09 23:50:34 +0900

    Add test code of editing html with jQuery

--- p5/sketch.js
+++ p5/sketch.js
@@ -14,7 +14,18 @@ function setup()
     dish.fill(255);
     dish.rect(0, 0, width, height);
 
+    // jQuery でHTMLを書き換えるテスト
+    foodstuffs = ["肉", "野菜"]
+    recipi = ["野菜を切る。", "肉を焼く。", "野菜を焼く。", "皿に盛る。"];
 
+    for (var i=0; i<foodstuffs.length; i++)
+    {
+        $("#foodstuff_list").append("<li>" + foodstuffs[i] + "</li>")
+    }
+    for (var i=0; i<recipi.length; i++)
+    {
+        $("#cooking").append("<li>" + recipi[i] + "</li>")
+    }
 }
 
 
