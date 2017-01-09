4d7e3261024f5c97bed2b176a1b58afaba2eefdc
daupet <ptjpwm992@gmail.com>
2017-01-09 23:49:20 +0900

    Add canvas

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,5 +1,19 @@
+var canvas;
+var dish; // 完成画像を描画するためのグラフィックバッファ
+
 function setup()
 {
+    // キャンバスを作成する
+    canvas = createCanvas(400, 250);
+    canvas.id("p5_canvas");
+    canvas.parent("canvas_container");
+    canvas.style("background-color", "white");
+
+    // 完成画像を描画するためのバッファを作成する
+    dish = createGraphics(width, height);
+    dish.fill(255);
+    dish.rect(0, 0, width, height);
+
 
 }
 
